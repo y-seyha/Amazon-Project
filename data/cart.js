dfb0868f39b7825c6aa62fba0d0b059828aba268
seyha yoeun <yoeunseyha11@gmail.com>
2025-10-31 15:05:48 +0700

    Move addToCart into cart.js

--- data/cart.js
+++ data/cart.js
@@ -1,1 +1,38 @@
 export const cart = [];
+
+export function addToCart(productId) {
+  let addedMessageTimeoutId; //using closure
+  let matchingItem;
+
+  cart.forEach((cartItem) => {
+    if (productId === cartItem.productId) {
+      matchingItem == cartItem;
+    }
+  });
+
+  const quantitySelector = document.querySelector(
+    `.js-quantity-selector-${productId}`
+  );
+  const quantity = Number(quantitySelector.value);
+
+  if (matchingItem) {
+    matchingItem.quantity += quantity;
+  } else {
+    cart.push({
+      productId,
+      quantity,
+    });
+  }
+
+  const addedMessage = document.querySelector(`.js-added-to-cart-${productId}`);
+  addedMessage.classList.add("added-to-cart-visible");
+
+  if (addedMessageTimeoutId) {
+    clearTimeout(addedMessageTimeoutId);
+  }
+  const timeoutId = setTimeout(() => {
+    addedMessage.classList.remove("added-to-cart-visible");
+  }, 2000);
+
+  addedMessageTimeoutId = timeoutId;
+}
