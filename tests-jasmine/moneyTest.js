ca5823c9930824f20974bd8ddee672e36c0dee23
seyha yoeun <yoeunseyha11@gmail.com>
2025-11-02 18:07:22 +0700

    Remove Defualt test and add money test

---
+++ tests-jasmine/moneyTest.js
@@ -0,0 +1,16 @@
+import { formatCurrency } from "../script/utils/money.js";
+
+// test suite for formatCurrency function
+describe("test suite formatCurrency", () => {
+  it("Convert Cents into dollars", () => {
+    expect(formatCurrency(2095)).toEqual("20.95"); // expected compared value
+  });
+
+  it("Convert zero cents into dollars", () => {
+    expect(formatCurrency(0)).toEqual("0.00");
+  });
+
+  it("Round up to the nearest cent", () => {
+    expect(formatCurrency(2000.5)).toEqual("20.01");
+  });
+});
---
+++ tests/tests.js
@@ -0,0 +1,22 @@
+import { formatCurrency } from "../script/utils/money.js";
+
+console.log("Convert cents into dollars");
+if (formatCurrency(2095) === "20.95") {
+  console.log('Test passed: formatCurrency(2095) === "20.95"');
+} else {
+  console.error('Test failed: formatCurrency(2095) !== "20.95"');
+}
+
+console.log("Convert zero cents into dollars");
+if (formatCurrency(0) === "0.00") {
+  console.log('Test passed: formatCurrency(0) === "0.00"');
+} else {
+  console.error('Test failed: formatCurrency(0) !== "0.00"');
+}
+
+console.log("Round up to the nearest cent ");
+if (formatCurrency(2000.5) === "20.01") {
+  console.log('Test passed: formatCurrency(2000.5) === "20.01"');
+} else {
+  console.error('Test failed: formatCurrency(2000.5) !== "20.01"');
+}
