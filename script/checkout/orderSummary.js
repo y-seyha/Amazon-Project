4194cfa94223839a1e3c7ce27f3569ae5e6fd422
seyha yoeun <yoeunseyha11@gmail.com>
2025-11-03 16:47:29 +0700

    orderSummaryTest

--- script/checkout/orderSummary.js
+++ script/checkout/orderSummary.js
@@ -28,7 +28,9 @@ export function renderOrderSummary() {
 
     const dateString = calculateDeliveryDate(deliveryOption);
 
-    cartSummaryHTML += `<div class="cart-item-container 
+    cartSummaryHTML += `
+    <div class="cart-item-container 
+    js-cart-item-container
   js-cart-item-container-${matchingProduct.id}">
     <div class="delivery-date">Delivery date: ${dateString}</div>
 
@@ -45,7 +47,8 @@ export function renderOrderSummary() {
         <div class="product-price">$${formatCurrency(
           matchingProduct.priceCents
         )}</div>
-        <div class="product-quantity">
+        <div class="product-quantity
+        js-product-quantity-${matchingProduct.id}">
             <span> Quantity: <span class="quantity-label 
             js-quantity-label-${matchingProduct.id}">
             ${cartItem.quantity}</span> </span>
@@ -59,6 +62,7 @@ export function renderOrderSummary() {
             <span class = "save-quantity-link link-primary js-save-link"
             data-product-id = "${matchingProduct.id}">Save</span>
             <span class="delete-quantity-link link-primary js-delete-link"
+            js-delete-link-${matchingProduct.id} 
             data-product-id= "${matchingProduct.id}" >
             Delete
             </span>
--- tests-jasmine/utils/moneyTest.js
+++ tests-jasmine/utils/moneyTest.js
@@ -1,4 +1,4 @@
-import { formatCurrency } from "../script/utils/money.js";
+import { formatCurrency } from "../../script/utils/money.js";
 
 // test suite for formatCurrency function
 describe("test suite formatCurrency", () => {
