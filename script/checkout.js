3d55723a2a72ec0241d756c649dfc0dc8ee8bb3b
seyha yoeun <yoeunseyha11@gmail.com>
2025-11-05 15:27:45 +0700

    Add backend practice file

--- script/checkout.js
+++ script/checkout.js
@@ -4,6 +4,7 @@ import { renderPaymentSummary } from "./checkout/paymentSummary.js";
 
 // import "../data/cart-class.js"; // Ensure cart-oop.js is loaded
 // import "../data/car.js";
+import "../data/backend-practice.js";
 
 renderCheckoutHeader();
 renderOrderSummary();
