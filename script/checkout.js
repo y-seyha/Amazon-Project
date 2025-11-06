1b9a5fce7491bac8837040be4574dac713f8dcf9
seyha yoeun <yoeunseyha11@gmail.com>
2025-11-06 18:09:48 +0700

    create promise.all

--- script/checkout.js
+++ script/checkout.js
@@ -2,33 +2,22 @@ import { renderCheckoutHeader } from "./checkout/checkoutHeader.js";
 import { renderOrderSummary } from "./checkout/orderSummary.js";
 import { renderPaymentSummary } from "./checkout/paymentSummary.js";
 import { loadProductsFetch } from "../data/products.js";
-import { loadCart } from "../data/cart.js";
+import { loadCart, loadCartFetch } from "../data/cart.js";
 // import "../data/cart-class.js"; // Ensure cart-oop.js is loaded
 // import "../data/car.js";
 // import "../data/backend-practice.js";
 
 async function loadPage() {
   try {
-    // throw 'error1';
-
-    await loadProductsFetch();
-
-    const value = await new Promise((resolve, reject) => {
-      // throw 'error2'
-
-      loadCart(() => {
-        // reject("error3");
-        resolve("value3");
-      });
-    });
+    await Promise.all([loadProductsFetch(), loadCartFetch()]);
   } catch (error) {
-    console.log("Error Caught ");
+    console.log("Unexpected error. Please try again later. ");
   }
-
   renderCheckoutHeader();
   renderOrderSummary();
   renderPaymentSummary();
 }
+
 loadPage();
 
 //Array of Promises //Wait for all to load and move to next step
